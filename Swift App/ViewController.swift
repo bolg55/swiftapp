7a58d07c0193f803a59a8088b0491d35466b0b7c
Kellen Bolger <bolger.kellen@gmail.com>
2018-09-04 22:17:19 -0400

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Kellen Bolger on 2018-09-04.
+//  Copyright © 2018 Kellen Bolger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
